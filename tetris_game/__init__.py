dd2b66468211ebca4bb2d3b1449d90b3fbb365fe
Cédric LOMBARD <cedric.lombard@cern.ch>
2020-04-25 19:32:16 +0200

    Modify Critical Error in manifest

--- tetris_game/__init__.py
+++ tetris_game/__init__.py
@@ -5,7 +5,7 @@
     Ce module implémente une version simple du Jeu Tetris
 """
 
-__version__ = "0.0.5"
+__version__ = "0.0.6"
 
 from tetris_game.core import Square, Line, Shape, Menu, TetrisFrame, TetrisGame
 from tetris_game.constantes import *
